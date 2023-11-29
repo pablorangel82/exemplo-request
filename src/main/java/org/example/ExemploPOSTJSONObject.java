b192b4c288ec13ec39a115bdef79b3b4732e70f9
Pablo Rangel <pablorangel@gmail.com>
2023-11-29 18:23:57 -0300

    adição de exemplo de POST com JSONObject

--- src/main/java/org/example/ExemploPOSTJSONObject.java
+++ src/main/java/org/example/ExemploPOSTJSONObject.java
@@ -1,29 +1,27 @@
 package org.example;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.ProtocolException;
 import java.net.URL;
 
-public class ExemploPOST {
+public class ExemploPOSTJSONObject {
     public static void main(String[] args) throws IOException {
         URL urlObj = new URL("https://jsonplaceholder.typicode.com/posts");
         HttpURLConnection conexao = (HttpURLConnection)urlObj.openConnection();
         conexao.setRequestProperty("Accept", "application/json");
         conexao.setDoOutput(true);
         conexao.setRequestMethod("POST");
-        User user = new User();
-        user.setId(1);
-        user.setUserId(1);
-        user.setTitle("delectus aut autem");
-        user.setCompleted(true);
-        ObjectMapper objectMapper = new ObjectMapper();
-        String json = objectMapper.writeValueAsString(user);
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("id",1);
+        jsonObject.put("userId",1);
+        jsonObject.put("title","delectus aut autem" );
+        jsonObject.put("completed",true);
+        String json = jsonObject.toString();
         conexao.getOutputStream().write(json.getBytes());
 
         BufferedReader in = new BufferedReader(new InputStreamReader(conexao.getInputStream()));
